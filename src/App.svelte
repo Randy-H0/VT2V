bf971ff032e8b06393f89e44b66f74d876266dc5
gftertime <creamyslipsbooos567890@gmail.com>
2022-03-08 19:00:09 +0300

    hmm

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+
+</script>
+
+<main>
+	<h1>hello</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+
+		text-align: center;
+		text-align-last: left;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		font-family: 'Dosis', sans-serif;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 3em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
